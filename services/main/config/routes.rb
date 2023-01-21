519f9488f5af5c462e203b5d340255609fa2e505
Maciej Panek <78555723+mpaneq@users.noreply.github.com>
2023-01-21 15:03:33 +0100

    Main views (#6)
    * feat: added new views, extended views' functionality
    
    Co-authored-by: Maciej Panek <maciejpa@student.agh.edu.pl>

--- services/main/config/routes.rb
+++ services/main/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "application#hello"
+  resources :search, only: [:index, :show]
+  resources :products, only: [:show, :create]
+  resources :orders
+  resources :payments
+  resources :invoices
 end
